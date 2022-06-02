986578d23925024f79d6a174e9244346e16cfa5c
Jaye Dong <1215859224@qq.com>
2022-06-02 09:26:52 +0800

    add lc283

---
+++ 283.移动零.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int slowIndex = 0;
+        int fastIndex;
+        for(fastIndex = 0; fastIndex < (int)nums.size(); fastIndex++){
+            /* 解题的关键在于理解双指针的本质 */
+            /* 本质上慢指针可以看作是一个新的数组，把快指针满足条件的赋值过去 */
+            if(nums[fastIndex] != 0){
+                nums[slowIndex++] = nums[fastIndex];
+            }
+        }
+        /* 并不需要真的移动，0作为一个特殊值，可以直接赋给最后几个元素 */
+        while(slowIndex < fastIndex){
+            nums[slowIndex++] = 0;
+        }
+    }
+};
+
+int main(){
+    Solution solution;
+    vector<int> a = {0,1,0,3,12};
+    solution.moveZeroes(a);
+    for(int i = 0; i < (int)a.size(); i++){
+        cout << a[i] << ", ";
+    }
+    cout << endl;
+    return 0;
+}
