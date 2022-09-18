f81262a5a25a4934c7f0ba01e7ea5b48bab9a49d
Jaye Dong <1215859224@qq.com>
2022-09-18 20:21:05 +0800

    update lc383

--- 383.赎金信.cpp
+++ 383.赎金信.cpp
@@ -1,20 +1,16 @@
 #include <string>
 #include <iostream>
 using namespace std;
-class Solution {
+class Solution{
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        if(ransomNote.size() > magazine.size()) return false;
-        int hash_ransomNote[128] = {0};
-        int hash_magazine[128] = {0};
-        for(int i = 0; i < (int)ransomNote.size(); i++){
-            hash_ransomNote[(size_t)ransomNote[i]]++;
+        int hash[26] = {0};
+        for(char c : magazine){
+            hash[c - 'a']++;
         }
-        for(int i = 0; i < (int)magazine.size(); i++){
-            hash_magazine[(size_t)magazine[i]]++;
-        }
-        for(int i = 0;i < 128;i++){
-            if(hash_magazine[i] < hash_ransomNote[i]){
+        for(char c : ransomNote){
+            hash[c - 'a']--;
+            if(hash[c - 'a'] < 0){
                 return false;
             }
         }
