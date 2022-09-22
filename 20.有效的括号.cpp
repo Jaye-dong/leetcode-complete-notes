ad9a41a2992e4a76eb53c4ec106a4f9d56c96192
Jaye Dong <1215859224@qq.com>
2022-09-22 09:28:01 +0800

    add lc20

---
+++ 20.有效的括号.cpp
@@ -0,0 +1,27 @@
+#include <stack>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Solution{
+public:
+    bool isValid(string s){
+        stack<char> st;
+        for(char c : s){
+            if(c == '(') st.push(')');
+            else if(c == '[') st.push(']');
+            else if(c == '{') st.push('}');
+            else if( st.empty() || c != st.top()) return false;
+            else if(c == ')' || c == ']' || c == '}') st.pop();
+        }
+        return st.empty();
+    }
+};
+
+int main(){
+    string s;
+    cin >> s;
+    Solution solution;
+    cout << solution.isValid(s);
+    return 0;
+}
