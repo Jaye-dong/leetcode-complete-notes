258c9f913f146002ff106a39554d045aa22118da
Jaye Dong <1215859224@qq.com>
2022-09-26 16:50:50 +0800

    update lc144 lc94

--- 144.二叉树的前序遍历.cpp
+++ 144.二叉树的前序遍历.cpp
@@ -1,45 +1,63 @@
 #include <iostream>
 #include <vector>
-
-using namespace std;
+#include <stack>
+#include <unordered_map>
 
 struct TreeNode{
     int val;
     TreeNode * left;
     TreeNode * right;
-    TreeNode(int _val): val(_val), left(nullptr), right(nullptr){}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr){}
 };
+using namespace std;
 
 class Solution{
 public:
-    void dfs(TreeNode *cur, vector<int> &res){
-        if(cur == nullptr){
-            return;
-        }
-        res.push_back(cur->val);
-        dfs(cur->left, res);
-        dfs(cur->right, res);
-    }
     vector<int> preorderTraversal(TreeNode* root) {
-        
+        stack<TreeNode*> st;
         vector<int> res;
-        dfs(root, res);
+        if(root == nullptr) return res;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            st.pop();
+            res.push_back(node->val);
+            if(node->right) st.push(node->right);
+            if(node->left) st.push(node->left);
+        }
         return res;
     }
 };
 
+
 int main(){
-    TreeNode *T = new TreeNode(1);
-    T->left = new TreeNode(2);
-    T->right = new TreeNode(3);
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    int i = 0;
+    unordered_map<int, TreeNode*> hash;
+    hash[i] = new TreeNode(nums[i]);
+    while(i < (int)nums.size()){
+        if(i * 2 + 1 < (int)nums.size()){
+            hash[i*2+1] = new TreeNode(nums[i*2+1]);
+            hash[i]->left = hash[i*2+1];
+        }
+        if(i * 2 + 2 < (int)nums.size()){
+            hash[i*2+2] = new TreeNode(nums[i*2+2]);
+            hash[i]->right = hash[i*2+2];
+        }
+        i++;
+    }
+    TreeNode * root = hash[0];
+
     Solution solution;
-    vector<int> res = solution.preorderTraversal(T);
+    vector<int> res = solution.preorderTraversal(root);
     for(int a : res){
         cout << a << " ";
     }
     cout << endl;
     return 0;
 }
-
-
-
--- 94.二叉树的中序遍历.cpp
+++ 94.二叉树的中序遍历.cpp
@@ -1,35 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <unordered_map>
-
-using namespace std;
 
 struct TreeNode{
     int val;
-    TreeNode * left;
+    TreeNode *left;
     TreeNode * right;
     TreeNode(int x): val(x), left(nullptr), right(nullptr){}
 };
+using namespace std;
+
 class Solution{
 public:
-    void dfs(TreeNode* cur, vector<int> &res){
-        if(cur == nullptr){
-            return;
-        }
-        dfs(cur->left, res);
-        res.push_back(cur->val);
-        dfs(cur->right, res);
-    }
+
     vector<int> inorderTraversal(TreeNode* root){
         vector<int> res;
-        dfs(root, res);
+        stack<TreeNode*> st;
+        if(root == nullptr) return res;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode * node = st.top();
+            if(node != nullptr){
+                st.pop();
+                if(node->right){
+                    st.push(node->right);
+                }
+                if(node->left){
+                    st.push(node->left);
+                }
+                st.push(node);
+                st.push(nullptr);
+                
+            }else{
+                st.pop();
+                TreeNode *node = st.top();
+                st.pop();
+                res.push_back(node->val);
+            }
+        }
         return res;
     }
 };
-
-
 int main(){
-
     int n;
     cin >> n;
     vector<int> nums(n);
@@ -39,21 +52,22 @@ int main(){
     int i = 0;
     unordered_map<int, TreeNode*> hash;
     TreeNode * root;
-    while(i < (int)nums.size()){
+    while(i < n){
         if(i == 0){
             hash[i] = new TreeNode(nums[i]);
             root = hash[i];
         }
-        if(i*2+1 < (int)nums.size()){
+        if(i*2+1 < n){
             hash[i*2+1] = new TreeNode(nums[i*2+1]);
             hash[i]->left = hash[i*2+1];
         }
-        if(i*2+2 < (int)nums.size()){
+        if(i*2+2 < n){
             hash[i*2+2] = new TreeNode(nums[i*2+2]);
             hash[i]->right = hash[i*2+2];
         }
         i++;
     }
+
     Solution solution;
     vector<int> res = solution.inorderTraversal(root);
     for(int a : res){
