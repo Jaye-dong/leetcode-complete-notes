51cad048b17aa77b048667555baa3ae47ac257e5
Jaye Dong <1215859224@qq.com>
2022-10-04 17:03:26 +0800

    update lc101

--- 101.对称二叉树.cpp
+++ 101.对称二叉树.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <stack>
 #include <unordered_map>
 
 using namespace std;
@@ -12,21 +12,40 @@ struct TreeNode{
     TreeNode(int x): val(x), left(nullptr), right(nullptr){}
 };
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution{
 public:
-    bool compare(TreeNode* leftNode, TreeNode* rightNode){
-        /* exclude null situation */
-        if(!leftNode && !rightNode) return true;
-        if(!leftNode || !rightNode || (leftNode->val != rightNode->val)){
-            return false;
-        }
-        bool outside = compare(leftNode->left, rightNode->right);
-        bool inside = compare(leftNode->right, rightNode->left);
-        bool isSame = outside && inside;
-        return isSame;
-    }
     bool isSymmetric(TreeNode* root) {
-        bool res = compare(root->left, root->right);
+        bool res = true;
+        stack<TreeNode*> st;
+        if(root == nullptr) return res;
+        st.push(root->left);
+        st.push(root->right);
+        while(!st.empty()){
+            TreeNode * leftNode = st.top(); st.pop();
+            TreeNode * rightNode = st.top(); st.pop();
+
+            if(!leftNode && !rightNode){
+                continue;
+            }
+            if(!leftNode || !rightNode || (leftNode->val != rightNode->val)){
+                return false;
+            }
+            st.push(leftNode->left);
+            st.push(rightNode->right);
+            st.push(leftNode->right);
+            st.push(rightNode->left);
+        }
         return res;
     }
 };
