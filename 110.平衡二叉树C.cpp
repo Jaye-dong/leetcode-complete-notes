a4cf1c7d48be240e3a7ff3e15d559cae1160313d
Jaye Dong <1215859224@qq.com>
2022-10-04 22:53:40 +0800

    add lc110

---
+++ 110.平衡二叉树C.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+};
+
+class Solution{
+public:
+    int getHeight(TreeNode* root){
+        if(root == nullptr) return 0;
+        /* postorder */
+        int leftHeight = getHeight(root->left);
+        if(leftHeight == -1) return -1;
+        int rightHeight = getHeight(root->right);
+        if(rightHeight == -1) return -1;
+        int res;
+        if(abs(leftHeight - rightHeight) > 1){
+            res = -1;
+        }else{
+            res = 1 + max(leftHeight, rightHeight);
+        }
+        return res;
+    }
+    bool isBalanced(TreeNode* root) {
+        if(getHeight(root) == -1){
+            return false;
+        }else{
+            return true;
+        }
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    int i = 0;
+    unordered_map<int, TreeNode*> hash;
+    hash[i] = new TreeNode(nums[i]);
+    TreeNode * root = hash[i];
+    while(i < n){
+        int L = i * 2 + 1;
+        int R = L + 1;
+        if(L < n){
+            hash[L] = new TreeNode(nums[L]);
+            hash[i]->left = hash[L];
+        }
+        if(R < n){
+            hash[R] = new TreeNode(nums[R]);
+            hash[i]->right = hash[R];
+        }
+        i++;
+    }
+
+    Solution solution;
+    int res = solution.isBalanced(root);
+    cout << res << endl;
+    return 0;
+}
