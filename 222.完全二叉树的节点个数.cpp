a9409a4104e8061f63f1177f9d492945e7b34959
Jaye Dong <1215859224@qq.com>
2022-10-04 21:41:09 +0800

    add lc222

---
+++ 222.完全二叉树的节点个数.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode * left;
+    TreeNode * right;
+    TreeNode(int x):val(x), left(nullptr), right(nullptr){}
+};
+
+class Solution{
+public:
+    int getNodesNum(TreeNode * root){
+        if(root == nullptr) return 0;
+        int leftNodes = getNodesNum(root->left);
+        int rightNodes = getNodesNum(root->right);
+        int TreeNum = 1 + leftNodes + rightNodes;
+        return TreeNum;
+    }    
+    int countNodes(TreeNode * root){
+        return getNodesNum(root);
+    }
+};
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++){
+        cin >> nums[i];
+    }
+    int i = 0;
+    unordered_map<int, TreeNode*> hash;
+    hash[i] = new TreeNode(nums[i]);
+    TreeNode * root = hash[i];
+    while(i < n){
+        int L = i * 2 + 1;
+        int R = L + 1;
+        if(L < n){
+            hash[L] = new TreeNode(nums[L]);
+            hash[i]->left = hash[L];
+        }
+        if(R < n){
+            hash[R] = new TreeNode(nums[R]);
+            hash[i]->right = hash[R];
+        }
+        i++;
+    }
+
+    Solution solution;
+    int res = solution.countNodes(root);
+    cout << res << endl;
+    return 0;
+}
