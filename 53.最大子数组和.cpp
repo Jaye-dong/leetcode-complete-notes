e327b7481f25e57bfa6682c87a7a476a8281cf93
Jaye Dong <1215859224@qq.com>
2023-02-23 11:03:28 +0800

    add lc53

--- 53.最大子数组和.cpp
+++ 53.最大子数组和.cpp
@@ -1,46 +1,37 @@
-#include <vector>
 #include <iostream>
-
+#include <vector>
+#include <climits>
 
 using namespace std;
 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums, const string method = "tx") {
-        if(nums.size() == 0) return 0;
-        int result = nums[0];
-        if(method == "tx"){
-            int count = 0;
-            for(int i = 0; i < nums.size(); i++){
-                count += nums[i];
-                if(count > result){
-                    result = count;
-                }
-                if(count < 0){
-                    count = 0;
-                }
+    int maxSubArray(vector<int>& nums) {
+        int sum = INT_MIN;
+        int current_sum = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            current_sum += nums[i];
+            /* 记录过程中最大值 */
+            if (current_sum > sum) {
+                sum = current_sum;
             }
-        }else if(method == "dp"){
-            int pre = nums[0];
-            for(int i = 1; i < nums.size(); i++){
-                pre = max(pre + nums[i], nums[i]);
-                result = max(pre, result);
+            /* 注意这两个要分开判断，否则会无法覆盖小于0的情况 */
+            /* 如果小于0，就设置为0 */
+            if (current_sum < 0) {
+                current_sum = 0;
             }
         }
-
-        return result;
+        return sum;
     }
-
 };
 
 int main(){
     int n;
     cin >> n;
     vector<int> nums(n);
-    for(int i = 0; i < n; i++){
+    for(int i = 0; i < n; i++) {
         cin >> nums[i];
     }
-    Solution solution;
-    cout << solution.maxSubArray(nums, "dp") << endl;
+    cout << Solution().maxSubArray(nums) << endl;
     return 0;
 }
