b5597b0eabd6d4827a6080ed5b957fb6717619c8
Jaye Dong <1215859224@qq.com>
2023-03-30 12:29:34 +0800

    modify lc70 add mehod2

--- 70.爬楼梯.cpp
+++ 70.爬楼梯.cpp
@@ -27,6 +27,26 @@ public:
 
         return dp[n];
     }
+
+    int climbStairs(int n, int method2) {
+        /* 完全背包解法 */
+        /* 1. dp[i]：爬到第i阶有多少种方法 */
+        /* 2. dp[i] += dp[i - nums[j]] */
+        /* 3. dp[0] = 1 */
+        /* 4. 排列，先包后物 */
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        int m = 2;
+        for(int i = 0; i <= n; i++) {
+            for (int j = 1; j <= 2; j++) {
+                if(i >= j){
+                    dp[i] += dp[i - j];
+                }
+            }
+        }
+        /* 5. 没时间，先不验啦 */
+        return dp[n];
+    }
 };
 
 
@@ -34,5 +54,6 @@ int main(){
     int n;
     cin >> n;
     cout << Solution().climbStairs(n) << endl;
+    cout << Solution().climbStairs(n,0) << endl;
     return 0;
 }
